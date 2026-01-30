7217c900bb1e4bd5e8365194ffb79c56890d74c7
prakash95150 <gnana9515@gmail.com>
2026-01-30 13:24:57 +0530

    Color Picker Using Javascript- Basic Project

---
+++ index.js
@@ -0,0 +1,20 @@
+let colorSpan = document.getElementById("color-span");
+let backgroundContainer = document.getElementById("background-container");
+
+
+function changeBackgroundButton1() {
+   colorSpan.textContent = "#e0e0e0";
+   backgroundContainer.style.backgroundColor = "#e0e0e0";
+}
+function changeBackgroundButton2() {
+   colorSpan.textContent = "#6fcf97";
+   backgroundContainer.style.backgroundColor = "#6fcf97";
+}
+function changeBackgroundButton3() {
+    colorSpan.textContent = "#56ccf2";
+    backgroundContainer.style.backgroundColor = "#56ccf2";
+}
+function changeBackgroundButton4() {
+    colorSpan.textContent = "#bb6bd9";
+    backgroundContainer.style.backgroundColor = "#bb6bd9";
+}
